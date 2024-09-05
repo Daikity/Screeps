6acbbd1349a9ed9d6225946833796a6300471afa
Igor Edison <igor.edison@kiwitaxi.com>
2024-09-05 22:38:18 +0300

    deliverymans. preliminary logic

--- src/prototypes/creep/role/deliverymans.js
+++ src/prototypes/creep/role/deliverymans.js
@@ -1,14 +1,24 @@
 Creep.prototype.transferToStorage = function () {
-  const storage = this.room.storage
-  if(storage) {
-    if(this.pos.isNearTo(storage)) {
-      this.transfer(storage, RESOURCE_ENERGY)
-      this.memory.working = false;
+  if (this.store.getFreeCapacity() > 0 && !this.memory.work) {
+    // const target = ??? TODO: need target object for transfer operation
+    // if (target) {
+    //   this.moveToAndCollect(target);
+    // }
+
+    // TODO: add target source for transfer to storage
+  } else {
+    if (!this.store.getUsedCapacity()) {
+      this.memory.work = false
     } else {
-      this.moveTo(storage);
+      this.memory.work = true
+      const storage = this.room.storage
+      if(storage) {
+        if(this.pos.isNearTo(storage)) {
+          this.transfer(storage, RESOURCE_ENERGY)
+        } else {
+          this.moveTo(storage);
+        }
+      }
     }
-    return true;
   }
-  this.memory.working = false;
-  return false;
 }
