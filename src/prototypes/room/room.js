db481c524c398f452a68a3451167835a4609416e
Igor Edison <igor.edison@kiwitaxi.com>
2024-09-05 03:37:43 +0300

    update reed some positions

---
+++ src/prototypes/room/room.js
@@ -0,0 +1,37 @@
+Room.prototype.findContainersNear = function(creepFreeCapacity) {
+  return this.find(FIND_STRUCTURES, {
+    filter: (structure) => structure.structureType === STRUCTURE_CONTAINER &&
+      structure.store[RESOURCE_ENERGY] > 0 && structure.store[RESOURCE_ENERGY] > creepFreeCapacity &&
+      !this.find(FIND_HOSTILE_CREEPS, 5).length
+  });
+};
+
+Room.prototype.findLinksNear = function() {
+  return this.find(FIND_STRUCTURES, {
+    filter: (structure) => structure.structureType === STRUCTURE_LINK &&
+      structure.store[RESOURCE_ENERGY] > 0 &&
+      this.find(FIND_MY_SPAWNS, 3).length > 0 &&
+      !this.find(FIND_HOSTILE_CREEPS, 5).length
+  });
+};
+
+Room.prototype.findDroppedResources = function(resourceType) {
+  return this.find(FIND_DROPPED_RESOURCES, {
+    filter: (resource) => resource.resourceType === resourceType &&
+      !this.find(FIND_HOSTILE_CREEPS, 5).length
+  });
+};
+
+Room.prototype.findTombstonesWithResource = function(resourceType) {
+  return this.find(FIND_TOMBSTONES, {
+    filter: (tombstone) => tombstone.store[resourceType] > 0 &&
+      !this.find(FIND_HOSTILE_CREEPS, 5).length
+  });
+};
+
+Room.prototype.findRuinsWithResource = function(resourceType) {
+  return this.find(FIND_RUINS, {
+    filter: (ruin) => ruin.store[resourceType] > 0 &&
+      !this.find(FIND_HOSTILE_CREEPS, 5).length
+  });
+};
--- src/prototypes/room/room.position.js
+++ src/prototypes/room/room.position.js
@@ -1,57 +1,11 @@
-const { getWalkablePosition, getFreePosition } = require('../../libs')
+RoomPosition.prototype.findOpenSource = function(resourceType) {
+  const source = this.findClosestByPath(FIND_SOURCES, {
+    filter: (s) => !s.pos.lookFor(LOOK_CREEPS).length &&
+      !this.findInRange(FIND_HOSTILE_CREEPS, 5).length
+  });
 
-RoomPosition.prototype.getNearbyPosition = function () {
-  const positions = [], startX = this.x || 1, startY = this.y || 1
-
-  positions.push(new RoomPosition(startX - 1, startY, this.roomName));
-  positions.push(new RoomPosition(startX + 1, startY, this.roomName));
-  positions.push(new RoomPosition(startX, startY - 1, this.roomName));
-  positions.push(new RoomPosition(startX, startY + 1, this.roomName));
-
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      if (i != 0 || j != 0) {
-        positions.push(new RoomPosition(startX + i, startY + j, this.roomName));
-      }
-    }
+  if(source) {
+    return source;
   }
-
-  return positions
-}
-
-RoomPosition.prototype.getPositionForContainer = function () {
-  const nearPosition = this.getNearbyPosition()
-  const containers = []
-
-  if(Memory.sources) {
-    for (const room in Memory.sources) {
-      if (Game.rooms[room]) {
-        const containersInRoom = Game.rooms[room].find(FIND_STRUCTURES, {
-        filter: (structure) => structure.structureType == STRUCTURE_CONTAINER
-      })
-      containers.push(...containersInRoom)
-      }
-    }
-  }
-
-  const findContainersAroundSource = _.find(nearPosition, position => {
-    return _.find(containers, container => container.pos.isEqualTo(position)) || false;
-  })
-  return findContainersAroundSource || false
-}
-
-RoomPosition.prototype.getOpenPositionForContainer = function () {
-  if (!this.lookFor(LOOK_CREEPS).length) {
-    return this
-  }
-  return false
-}
-
-RoomPosition.prototype.getOpenPosition = function () {
-  const nearbyPosition = this.getNearbyPosition()
-  const terrain = Game.map.getRoomTerrain(this.roomName);
-  const walkablePosition = getWalkablePosition(terrain, nearbyPosition, TERRAIN_MASK_WALL)
-  const freePosition = getFreePosition(walkablePosition)
-
-  return freePosition
-}
+  return
+};
